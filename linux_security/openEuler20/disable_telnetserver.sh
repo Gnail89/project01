82ebcee3a538bdbcfc0e20a58a90aea898e9a83c
Gnail89 <Gnail89@users.noreply.github.com>
2023-03-27 17:45:47 +0800

    update

--- linux_security/openEuler20/disable_telnetserver.sh
+++ linux_security/openEuler20/disable_telnetserver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 systemctl disable telnet.socket
-systemctl status telnet.socket
+systemctl is-active telnet.socket
 if [ $? -eq 0 ]; then
     systemctl stop telnet.socket
 fi
---
+++ linux_security/openEuler22/setup_rsyslog.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+f_rsyslog='/etc/rsyslog.conf'
+f_remotelog='1.1.1.1'
+
+restart_rsyslog(){
+    systemctl is-active rsyslog
+    if [ $? -eq 0 ]; then
+        systemctl restart rsyslog
+    fi
+}
+
+if [ -w ${f_rsyslog} ]; then
+    cp ${f_rsyslog}{,.bak.$(date +%s)}
+    # authpriv.*
+    if [ $(grep -E '^authpriv.\*.*/var/log/secure' ${f_rsyslog} |wc -l) -eq 0 ]; then
+        echo 'authpriv.*      /var/log/secure' >> ${f_rsyslog}
+    fi
+    # crontab logging
+    if [ $(grep -E '^cron.\*.*/var/log/cron' ${f_rsyslog} |wc -l) -eq 0 ];then
+        echo 'cron.*      /var/log/cron' >> ${f_rsyslog}
+    fi
+    # errors logging
+    if [ $(grep -E '^\*.err.*/var/log/errors' ${f_rsyslog} |wc -l) -eq 0 ];then
+        echo '*.err      /var/log/errors' >> ${f_rsyslog}
+    fi
+    # Remote Logging
+    if [ $(grep -E "^auth.info;user.notice.*@*${f_remotelog}" ${f_rsyslog} |wc -l) -eq 0 ]; then
+        echo "auth.info;user.notice      @${f_remotelog}" >> ${f_rsyslog}
+    fi
+    # reload service
+    restart_rsyslog
+fi
